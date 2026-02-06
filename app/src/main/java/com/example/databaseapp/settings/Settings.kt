667f0bf562f19aaf958c06711292a52e0ce57e8f
torwanat <nawrotm37@gmail.com>
2026-02-06 13:57:21 +0200

    Add image storage

--- app/src/main/java/com/example/databaseapp/settings/Settings.kt
+++ app/src/main/java/com/example/databaseapp/settings/Settings.kt
@@ -1,6 +1,8 @@
 package com.example.databaseapp.settings
 
+import android.content.Context
 import android.net.Uri
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
@@ -8,10 +10,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -22,9 +28,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
@@ -32,6 +40,8 @@ import coil.request.ImageRequest
 import com.example.databaseapp.AppViewModelProvider
 import com.example.databaseapp.ui.theme.DatabaseAppTheme
 import com.example.navigationapp.Routes
+import java.io.File
+import java.io.OutputStream
 
 @Composable
 fun SettingsScreen(
@@ -83,14 +93,36 @@ fun SettingsScreenBody(
 
 @Composable
 fun PhotoContainer(){
-    var photoUri: Uri? by remember { mutableStateOf(null) }
-    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri: Uri? ->
-        photoUri = uri
+    val savedImageName = "profile_image.jpg"
+    val context = LocalContext.current
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()) { uri: Uri? ->
+        uri?.let {
+            context.savePhoto(it, savedImageName)
+        }
     }
 
     Column (
         modifier = Modifier.padding(16.dp),
     ) {
+        if (File(context.filesDir, savedImageName).exists()) {
+            val savedPhotoUri = File(context.filesDir, savedImageName).toUri()
+            Log.d("PhotoContainer", savedPhotoUri.toString())
+            val painter = rememberAsyncImagePainter(
+                ImageRequest.Builder(LocalContext.current)
+                    .data(savedPhotoUri)
+                    .build()
+            )
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(200.dp)
+                    .padding(10.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
         Button(
             modifier = Modifier.align(Alignment.CenterHorizontally),
             onClick = {
@@ -99,17 +131,18 @@ fun PhotoContainer(){
             Text(text = "Choose a photo")
         }
 
-        if (photoUri != null) {
-            val painter = rememberAsyncImagePainter(
-                ImageRequest.Builder(LocalContext.current)
-                    .data(data = photoUri).build()
-            )
-            Image(
-                painter = painter,
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-            )
-        }
-
     }
+}
+
+fun Context.savePhoto(photoUri: Uri, fileName: String) {
+    val inputStream = contentResolver.openInputStream(photoUri)
+    val outputStream: OutputStream = openFileOutput(fileName, Context.MODE_PRIVATE)
+    inputStream?.use { input ->
+        outputStream.use { output ->
+            Log.d("PhotoContainer", "It works")
+            input.copyTo(output)
+        }
+    }
+    inputStream?.close()
+    outputStream.close()
 }
